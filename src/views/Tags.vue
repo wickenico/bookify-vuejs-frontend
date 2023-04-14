f9af25585c49c2bd6288b72ee50a93fbd2320b7d
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2023-04-15 00:09:21 +0200

    fix: Modify Tag view to delete tags and show count

--- src/views/Tags.vue
+++ src/views/Tags.vue
@@ -10,26 +10,42 @@
         <div v-if="loading">
             <p>Loading tags...</p>
         </div>
-        <div v-else-if="filteredTags.length">
-            <div v-for="tag in filteredTags" :key="tag.id" class="tag">
+        <div v-else-if="tagsLoaded && filteredTags.length">
+            <div v-for="tag in filteredTags" :key="tag.id">
                 <router-link :to="{ name: 'TagDetails', params: { id: tag.id } }">
-                    <h2>{{ tag.name }}</h2>
+                    <div class="project">
+                        <div class="actions">
+                            <div>
+                                <h2>{{ tag.name }} ({{ tag.count }})</h2>
+                            </div>
+                            <div class="icons">
+                                <span v-if="tag.count === '0'" @click.prevent="deleteTag(tag)" class="material-icons">
+                                    delete
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </router-link>
+
             </div>
         </div>
-        <div v-else>
+        <div v-else-if="tagsLoaded">
             <p>Nothing found.</p>
         </div>
     </div>
 </template>
   
 <script>
+import { useToast } from "vue-toastification";
+const toast = useToast();
+
 export default {
     data() {
         return {
             tags: [],
             searchQuery: "",
             loading: false,
+            tagsLoaded: false,
         };
     },
     computed: {
@@ -54,14 +70,71 @@ export default {
             headers: headers,
         })
             .then((res) => res.json())
-            .then((data) => (this.tags = data))
+            .then((data) => {
+                const promises = data.map((tag) => {
+                    return fetch(this.apiUrl + "/tags/" + tag.id + "/count", {
+                        headers: headers,
+                    })
+                        .then((res) => res.text())
+                        .then((count) => {
+                            tag.count = count;
+                            return tag;
+                        });
+                });
+                Promise.all(promises).then((tags) => {
+                    this.tags = tags;
+                    this.tagsLoaded = true;
+                });
+            })
             .catch((err) => console.log(err.message))
             .finally(() => (this.loading = false));
     },
+    methods: {
+        async deleteTag(tag) {
+            if (this.confirm("Are you sure you want to delete this tag?")) {
+                const index = this.tags.indexOf(tag);
+                if (index > -1) {
+                    this.tags.splice(index, 1);
+                }
+                try {
+                    const headers = new Headers();
+                    if (sessionStorage.getItem('credentials')) {
+                        headers.append('Authorization', 'Basic ' + sessionStorage.getItem('credentials'));
+                        headers.append('Accept', 'application/json');
+                    }
+                    const response = await fetch(this.apiUrl + '/tags/' + tag.id,
+                        {
+                            method: 'DELETE',
+                            headers: headers
+                        })
+                    if (response.ok) {
+                        toast.success('Tag ' + tag.name + ' successfully deleted!', {
+                            position: "bottom-right",
+                            timeout: 2000,
+                            closeOnClick: true,
+                            pauseOnFocusLoss: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            draggablePercent: 0.6,
+                            showCloseButtonOnHover: false,
+                            closeButton: "button",
+                            icon: true,
+                            rtl: false
+                        });
+                        router.push({ path: '/tags' })
+                    }
+                } catch (error) {
+                    console.log(error.message)
+                }
+            }
+        },
+        confirm(message) {
+            return window.confirm(message);
+        },
+    }
 };
 </script>
   
-
 <style scoped>
 .search-container {
     max-width: 420px;
@@ -115,23 +188,39 @@ input:focus {
     box-shadow: 0 0 0 0.1rem teal;
 }
 
-.tag h2 {
+.project {
     margin: 20px auto;
     background: white;
     padding: 10px 20px;
     border-radius: 4px;
     box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
-    max-width: 600px;
-    cursor: pointer;
     color: #444;
+    max-width: 700px;
 }
 
-.tag h2:hover {
+.project:hover {
     color: white;
     background: teal;
 }
 
-.tag a {
-    text-decoration: none;
+.actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.material-icons {
+    font-size: 30px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+}
+
+.material-icons:hover {
+    color: black;
+}
+
+.icons {
+    margin-left: auto;
 }
 </style>
