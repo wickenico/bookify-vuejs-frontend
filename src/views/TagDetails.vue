b3823e1c7abd12073d3793d19ab2729e8c3819f7
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2023-04-21 00:30:57 +0200

    fix: Add delete method to book list in TagDetails

--- src/views/TagDetails.vue
+++ src/views/TagDetails.vue
@@ -5,7 +5,18 @@
   <div v-if="books">
     <div v-for="book in books" :key="book.id" class="book">
       <router-link :to="{ name: 'BookDetails', params: { id: book.id } }">
-        <h2>{{ book.title }}</h2>
+        <div class="project">
+          <div class="actions">
+            <div>
+              <h2>{{ book.title }}</h2>
+            </div>
+            <div class="icons">
+              <span @click.prevent="deleteBookTag(book, tag)" class="material-icons">
+                delete
+              </span>
+            </div>
+          </div>
+        </div>
       </router-link>
     </div>
   </div>
@@ -15,6 +26,9 @@
 </template>
 
 <script>
+import { useToast } from "vue-toastification";
+const toast = useToast();
+
 export default {
   props: ['id'],
   data() {
@@ -43,6 +57,48 @@ export default {
       .then(data => this.tag = data)
       .catch(err => console.log(err.message));
 
+  },
+  methods: {
+    async deleteBookTag(book, tag) {
+      if (this.confirm("Are you sure you want to delete this BookTag?")) {
+        const index = this.books.indexOf(book);
+        if (index > -1) {
+          this.books.splice(index, 1);
+        }
+        try {
+          const headers = new Headers();
+          if (sessionStorage.getItem('credentials')) {
+            headers.append('Authorization', 'Basic ' + sessionStorage.getItem('credentials'));
+            headers.append('Accept', 'application/json');
+          }
+          const response = await fetch(this.apiUrl + '/books/' + book.id + '/tags/' + tag.id + '/delete',
+            {
+              method: 'DELETE',
+              headers: headers
+            })
+          if (response.ok) {
+            toast.success('BookTag ' + tag.name + ' successfully deleted!', {
+              position: "bottom-right",
+              timeout: 2000,
+              closeOnClick: true,
+              pauseOnFocusLoss: true,
+              pauseOnHover: true,
+              draggable: true,
+              draggablePercent: 0.6,
+              showCloseButtonOnHover: false,
+              closeButton: "button",
+              icon: true,
+              rtl: false
+            });
+          }
+        } catch (error) {
+          console.log(error.message)
+        }
+      }
+    },
+    confirm(message) {
+      return window.confirm(message);
+    },
   }
 }
 </script>
@@ -53,23 +109,39 @@ export default {
   color: teal;
 }
 
-.book h2 {
+.project {
   margin: 20px auto;
   background: white;
   padding: 10px 20px;
   border-radius: 4px;
   box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
   color: #444;
-  max-width: 600px;
-  cursor: pointer;
+  max-width: 700px;
 }
 
-.book h2:hover {
+.project:hover {
   color: white;
   background: teal;
 }
 
-.book a {
-  text-decoration: none;
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.material-icons {
+  font-size: 30px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+}
+
+.material-icons:hover {
+  color: black;
+}
+
+.icons {
+  margin-left: auto;
 }
 </style>
